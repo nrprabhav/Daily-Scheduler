adb2d7ba86ec8f3d7147882ef139896120cd0722
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-01-26 21:07:57 +0000

    Color timeblocks/store events in local storage and read it back on refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,37 @@
-let todayDate=moment().format("MMM Do, YYYY");
+if (localStorage.getItem("eventsList") == null) {
+    let eventsList = Array(9).fill("");
+    localStorage.setItem("eventsList", JSON.stringify(eventsList));
+}
+
+let todayDate = moment().format("MMM Do, YYYY");
 console.log(todayDate);
 $("#currentDay").text(todayDate);
 
-let startHr = moment("09","hh")
-let hourList = $("<ul>")
-for(let i=0;i<8;i++){
-    console.log(startHr.format("hh a"));
-    startHr = startHr.add(1,'h');
+function updateTable() {
+    console.log("Hi");
+    let startTime = moment("09", "hh");
+    let currTime = moment("11", "hh");
+    eventsList = JSON.parse(localStorage.getItem("eventsList"));
+    for (i = 0; i < 9; i++) {
+        console.log(eventsList[i]);
+        $("#timeBlocks").children().eq(i).children('input').val(eventsList[i]);
+        if (currTime.diff(startTime, "hours") > 0) {
+            $("#timeBlocks").children().eq(i).children('input').attr("class", "description past");
+        } else if (currTime.diff(startTime, "hours") < 0) {
+            $("#timeBlocks").children().eq(i).children('input').attr("class", "description future");
+        } else {
+            $("#timeBlocks").children().eq(i).children('input').attr("class", "description present");
+        }
+        startTime.add(1, 'hours');
+    }
 }
+
+$(".saveBtn").on("click", function (event) {
+    console.log(event.currentTarget.parentNode.children[1].value);
+    console.log(event.currentTarget.parentNode.id[0] + 1);
+    eventsList = JSON.parse(localStorage.getItem("eventsList"));
+    eventsList[Number(event.currentTarget.parentNode.id[0]-1)] = event.currentTarget.parentNode.children[1].value;
+    localStorage.setItem("eventsList", JSON.stringify(eventsList));
+})
+
+updateTable();
