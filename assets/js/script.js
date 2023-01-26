f3d1f21307df4f3f08c98c0d2424364b9b3522a4
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-01-26 19:33:42 +0000

    Add timeblocks on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 let todayDate=moment().format("MMM Do, YYYY");
 console.log(todayDate);
 $("#currentDay").text(todayDate);
+
+let startHr = moment("09","hh")
+let hourList = $("<ul>")
+for(let i=0;i<8;i++){
+    console.log(startHr.format("hh a"));
+    startHr = startHr.add(1,'h');
+}
