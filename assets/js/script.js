f8d6fa7ffa15f6f0e265fb9c53c8815abaa8d6d2
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-01-27 17:38:12 +0000

    Commented the JavaScript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,31 @@
+// Check local storage and see if there are some events stored. 
+// If not initialize with a empty event list
 if (localStorage.getItem("eventsList") == null) {
     let eventsList = Array(9).fill("");
     localStorage.setItem("eventsList", JSON.stringify(eventsList));
 }
 
+// Display today's date
 let todayDate = moment().format("MMM Do, YYYY");
 console.log(todayDate);
 $("#currentDay").text(todayDate);
 
 function updateTable() {
-    console.log("Hi");
+    // Update the colors of the timeblock based on time 
+    // Display events stored in memory on the timeblocks
+
     let startTime = moment("09", "hh");
-    let currTime = moment("11", "hh");
+    let currTime = moment();
     eventsList = JSON.parse(localStorage.getItem("eventsList"));
     for (i = 0; i < 9; i++) {
-        console.log(eventsList[i]);
+        // Display events stored in memory
         $("#timeBlocks").children().eq(i).children('input').val(eventsList[i]);
-        if (currTime.diff(startTime, "hours") > 0) {
+
+        // Color timeblocks based on time
+        let timeDiff = currTime.diff(startTime, "hours");
+        if (timeDiff > 0) {
             $("#timeBlocks").children().eq(i).children('input').attr("class", "description past");
-        } else if (currTime.diff(startTime, "hours") < 0) {
+        } else if (timeDiff < 0) {
             $("#timeBlocks").children().eq(i).children('input').attr("class", "description future");
         } else {
             $("#timeBlocks").children().eq(i).children('input').attr("class", "description present");
@@ -26,9 +34,8 @@ function updateTable() {
     }
 }
 
+// Click Event
 $(".saveBtn").on("click", function (event) {
-    console.log(event.currentTarget.parentNode.children[1].value);
-    console.log(event.currentTarget.parentNode.id[0] + 1);
     eventsList = JSON.parse(localStorage.getItem("eventsList"));
     eventsList[Number(event.currentTarget.parentNode.id[0]-1)] = event.currentTarget.parentNode.children[1].value;
     localStorage.setItem("eventsList", JSON.stringify(eventsList));
